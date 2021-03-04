562b5229bc057fd11a089e2ed0db4960bd17e208
user <user@lausds-MacBook.local>
2021-03-04 13:32:01 -0800

    isolated the lessons part

--- main.js
+++ main.js
@@ -27,12 +27,18 @@ Vue.component("prophets", {
             <dl class="description">
                 <dt>List the things he dealt with:</dt>
                     <dd> {{obstacle}} </dd>
-                <dt>List of his response during the course of his prophet-hood:</dt>
+                <dt>List of his character qualities and acts of holiness:</dt>
                     <dd> {{solution}} </dd>
-                <dt>His mission:</dt>
+                <dt>His earthly mission:</dt>
                     <dd> {{goal}} </dd>      
             </dl>
         </div>
+        <ul>
+        <label class="l-title">Achievements</label>
+        <li v-for="(item, index) in achievements">
+          {{item}}
+        </li>
+      </ul>
   
         <hr>
        <morokot :lessons=lessons :achievements=achievements></morokot>
@@ -40,19 +46,13 @@ Vue.component("prophets", {
 });
 
 Vue.component("morokot", {
-  props: ["lessons", "achievements"],
+  props: ["lessons"],
   template: `<div class="lessons">
 
     <ul>
       <label class="l-title">Teachings</label>
       <li v-for="(item, index) in lessons">
         <a href="#">{{item}} {{index}}</a>
-      </li>
-    </ul>
-    <ul>
-      <label class="l-title">Achievements</label>
-      <li v-for="(item, index) in achievements">
-        {{item}}
       </li>
     </ul>
   </div>`,
@@ -163,38 +163,21 @@ var app = new Vue({
   el: "#app",
   data() {
     return {
+      biography1: ["Buddha's story"],
       obstacle1:
         "Family expectations, social obligations, religious extremists, narrow-sightedness, conflicts involving selfishness, conflicts involving ego, conflicts involving greed",
       solution1:
         "Perform right actions, have a balanced viewpoint, study the lessons of nature, study how nature interacts, develop courage, develop good deeds, develop merit, develop compassion",
       goal1: "Conquer the mind, the root issues of life, end suffering",
+      achievements1: ["Not available at this time"],
 
+      biography2: ["Confucius' story"],
       obstacle2:
         "War, bloodshed, neglect by community, born of the lower class, deformities from birth, social awkwardness, desparate poverty, insecurity, social disconnection, ruling class oppression, weak followers, exile, taxes from government, being in the state of no choice, blunt honesty",
       solution2:
         "Creativity, apetite for knowledge, studied history, independent research, unsubscribe from public opinion, develop family love, captured the attention of ruling class men, correct bad conditions in society that create social problems",
       goal2:
         "Ending corruption, ending bloodshed, restoring peace to the land, finding a virtous ruler to accept him as a teacher, save the world",
-
-      obstacle3:
-        "The practice of useless idolatory, the insincere, the lacking in integrity, the oppressive teachers of religion and law, superstition, social hopelessness, inner confusion, social cruelity",
-      solution3:
-        "Impart parables, distinguish truth vs falsehood, free people to choose their own paths in life, make known the kingdom of soul vs the way of cheating man",
-      goal3:
-        "Fufill the true meaning of the Prophetic Law, restore men back into their spiritual lineage, brought faith and love into the way of life, undo the fallen state of mind",
-
-      obstacle4:
-        "Toxic dominance, petty disputes, carelessness, natural disasters, selfish polytheism, idol worship, child sacrfice, abandonment from family members, the anti common good for all, pursuit of worldly gain over righteousness, religious persecution, community prejudice, government forced isolation, starvation, victimization, physical abuse, mockery, ridicule, military invasion, worldly attachments",
-      solution4:
-        "Scientific ingenuity, trustworthiness, generosity to combat lacking, honor neighbors, timing, asking right questions, never speak a bad word in return, replace greed with compassion, replace selfishness with concern, answering questions mildly, staying true to father Abraham's goal, fighting always for the right cause, retaliation to aggressors when necessary, unity, staying humble after all outcomes, mercy toward enemies, forgiveness of enemies, maintain spiritual permenance until victory, praying 5 times a day, fasting during Ramadan, religious purification, removing foreign selfish ideas, enforcing spiritual standards - raising the bar of holiness in the world through proper etiquette, reveal truths of Islam by doing right deeds",
-      goal4: "Transform society to be upright and merciful",
-
-      biography1: ["Buddha's story"],
-      lesson1: ["lesson", "lesson", "lesson"],
-      achievements1: ["Not available at this time"],
-
-      biography2: ["Confucius' story"],
-      lesson2: ["lesson", "lesson", "lesson"],
       achievements2: [
         "safe streets - crime virtually disappeared",
         "good buisness - merchants no longer cheated their customers",
@@ -202,17 +185,33 @@ var app = new Vue({
       ],
 
       biography3: ["Jesus' story"],
-      lesson3: ["lesson", "lesson", "lesson"],
+      obstacle3:
+        "The practice of useless idolatory, the insincere, the lacking in integrity, the oppressive teachers of religion and law, superstition, social hopelessness, inner confusion, social cruelity",
+      solution3:
+        "Impart parables, distinguish truth vs falsehood, free people to choose their own paths in life, make known the kingdom of soul vs the way of cheating man",
+      goal3:
+        "Fufill the true meaning of the Prophetic Law, restore men back into their spiritual lineage, brought faith and love into the way of life, undo the fallen state of mind",
       achievements3: ["Not available at this time"],
-
       biography4: ["Muhammad's story"],
-      lesson4: ["lesson", "lesson", "lesson"],
+      obstacle4:
+        "Toxic dominance, petty disputes, carelessness, natural disasters, selfish polytheism, idol worship, child sacrfice, abandonment from family members, the anti common good for all, pursuit of worldly gain over righteousness, religious persecution, community prejudice, government forced isolation, starvation, victimization, physical abuse, mockery, ridicule, military invasion, worldly attachments",
+      solution4:
+        "Scientific ingenuity, trustworthiness, generosity to combat lacking, honor neighbors, timing, asking right questions, never speak a bad word in return, replace greed with compassion, replace selfishness with concern, answering questions mildly, staying true to father Abraham's goal, fighting always for the right cause, retaliation to aggressors when necessary, unity, staying humble after all outcomes, mercy toward enemies, forgiveness of enemies, maintain spiritual permenance until victory, praying 5 times a day, fasting during Ramadan, religious purification, removing foreign selfish ideas, enforcing spiritual standards - raising the bar of holiness in the world through proper etiquette, reveal truths of Islam by doing right deeds",
+      goal4: "Transform society to be upright and merciful",
       achievements4: [
         "women's rights - women in society were exalted to higher positions at a fast pace than ever seen before",
         "gratitude - his followers were admired for bringing peace everywhere they went and were welcomed in believing places",
         "influene - became head of state",
         "civics - established a social order",
       ],
+
+      lesson1: ["lesson", "lesson", "lesson"],
+
+      lesson2: ["lesson", "lesson", "lesson"],
+
+      lesson3: ["lesson", "lesson", "lesson"],
+
+      lesson4: ["lesson", "lesson", "lesson"],
     };
   },
 });
